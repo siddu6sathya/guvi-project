842533dfbad0c9f98f128fdac829100ea470edc4
siddu6sathya <siddarth.chandrasekaran96@gmail.com>
2023-05-09 13:59:41 +0000

    Added .dockerignore and .gitignore files

---
+++ build/precache-manifest.4ba71e560fba07b3b54fd9d2b20b91f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf32d73a50e25ff20170662223a650be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ecf2fc186a3485a09b22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/js/2.c71cdbdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c71cdbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf2fc186a3485a09b22",
+    "url": "/static/js/main.449e2496.chunk.js"
+  },
+  {
+    "revision": "205dba4f9959a084aa7c",
+    "url": "/static/js/runtime-main.c2a4c5c8.js"
+  }
+]);
